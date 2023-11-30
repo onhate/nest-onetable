18effcce89b73337178f1907359c265eb4b4fbc9
Marcelo Luiz Onhate <onhate@gmail.com>
2023-11-30 10:23:47 -0300

    feat: types and tests improvements

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  preset: '@shelf/jest-dynamodb',
   testEnvironment: 'node',
   restoreMocks: true,
   clearMocks: true,
--- src/onetable.spec.ts
+++ src/onetable.spec.ts
@@ -1,29 +1,47 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { Injectable } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { Entity } from 'dynamodb-onetable';
+import { EntityParametersForCreate } from 'dynamodb-onetable/dist/cjs/Model';
 import { OneModel, OnetableModule } from './index';
 
 @Injectable()
 export class OneModelX extends OneModel('OneModelX', {
-  pk: { type: String, value: '${id}', hidden: true },
-  sk: { type: String, value: 'latest', hidden: true },
-  id: { type: String, value: '${id}', required: true }
+  pk: { type: String, value: 'ModelX#', hidden: true },
+  sk: { type: String, value: 'ModelX#${id}', hidden: true },
+  id: { type: String, generate: 'ulid' },
+  name: { type: String, required: true },
+  description: { type: String, required: false }
 }) {}
+
+type TypeOfModelX = typeof OneModelX.fields;
+type ModelX = Entity<TypeOfModelX>
 
 @Injectable()
 class Service {
   constructor(public readonly model: OneModelX) {
   }
+
+  async create(props: EntityParametersForCreate<TypeOfModelX>): Promise<ModelX> {
+    return await this.model.create(props);
+  }
 }
 
 describe('OneTable', () => {
-  it('should instantiate custom models', async () => {
-    const module = await Test.createTestingModule({
+  async function setup() {
+    return await Test.createTestingModule({
       imports: [
         OnetableModule.register({
           global: false,
-          client: jest.mocked(DynamoDBClient),
-          name: 'test:table:name',
+          client: new DynamoDBClient({
+            endpoint: 'http://localhost:5002',
+            region: 'local',
+            credentials: {
+              accessKeyId: 'fake',
+              secretAccessKey: 'fake'
+            }
+          }),
+          name: 'onetable',
           partial: true,
           schema: {
             format: 'onetable:1.1.0',
@@ -39,8 +57,21 @@ describe('OneTable', () => {
     })
       .compile()
       .then(app => app.init());
+  }
 
-    const node = module.get(Service);
-    expect(node.model).toBeDefined();
+
+  it('should instantiate custom models', async () => {
+    const module = await setup();
+    const service = module.get(Service);
+    expect(service.model).toBeDefined();
+  });
+
+  it('should validate types', async () => {
+    const module = await setup();
+    const service = module.get(Service);
+
+    const result = await service.create({ name: 'one and only one table' });
+    expect(result.id).toHaveLength(26); // ulid
+    expect(result.name).toBe('one and only one table');
   });
 });
